9e2cac74c237f255f59bbec05daea86dc04c3d29
Marcelo Rodrigo <marcelorodrigo@users.noreply.github.com>
2025-01-10 09:22:22 +0100

    Faster startup app (#14)

--- src/main/java/com/marcelorodrigo/fit/GarminConnectWeightApplication.java
+++ src/main/java/com/marcelorodrigo/fit/GarminConnectWeightApplication.java
@@ -3,12 +3,13 @@ package com.marcelorodrigo.fit;
 import com.marcelorodrigo.fit.properties.KeepAliveProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EnableConfigurationProperties(KeepAliveProperties.class)
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GarminConnectWeightApplication {
 
     public static void main(String[] args) {
